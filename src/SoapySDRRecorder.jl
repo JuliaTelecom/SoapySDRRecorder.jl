1a03549d27491afac1e6df6c11933dfb828c13e5
Steve Kelly <kd2cca@gmail.com>
2022-12-15 16:45:15 -0500

    more cleanups

--- src/SoapySDRRecorder.jl
+++ src/SoapySDRRecorder.jl
@@ -5,7 +5,7 @@ using Unitful
 using BufferedStreams
 
 gc_bytes() = Base.gc_bytes()
-get_time_us() = trunc(Int, time()*1_000_000)
+get_time_us() = trunc(Int, time()*1_000_000) #microseconds is a reasonable measure
 
 """
     record(output_file; direct_buffer_access = false, timer_display = true,
@@ -16,10 +16,11 @@ get_time_us() = trunc(Int, time()*1_000_000)
 Record data from a SDR device to a file or IOBuffer.
 """
 function record(output::AbstractString;
-                direct_buffer_access = false, timer_display = true,
+                timer_display = true,
                 device::Union{Nothing, SoapySDR.Device}=nothing, #XXX: Make this KWargs
                 channels::Union{Nothing, AbstractArray{<:SoapySDR.Channel}}=nothing,
-                channel_configuration::Union{Nothing, Function}=nothing)
+                channel_configuration::Union{Nothing, Function}=nothing,
+                stream_type::Type{T}=Complex{Int16}) where T
 
     # open the first device
     device = if device === nothing
@@ -37,8 +38,7 @@ function record(output::AbstractString;
     else
         channels
     end
-    num_channels = 1
-    #num_channels = length(channels)
+    num_channels = length(channels)
 
     # run the channel configuration function
     if channel_configuration !== nothing
@@ -46,26 +46,21 @@ function record(output::AbstractString;
     end
 
     # set up a stream (complex floats)
-    format = Complex{Int16} #first(channels).native_stream_format
-    rxStream = SoapySDR.Stream(format, channels)
-
-    # create a re-usable buffer for rx samples
-    buffsz = rxStream.mtu
-    buffs = Ptr{format}[C_NULL] # pointer for direct buffer API
-    # Allocate some buffers
-    # concurrent ring buffer?
-    # for some reason the compiler does not constant prop the type
-    # TODO put in signature
-    buffers = [Vector{Complex{Int16}}(undef, buffsz) for _ in 1:num_channels]
-    buff_ptrs = pointer(map(pointer, buffers))
+    rxStream = SoapySDR.Stream(T, channels)
 
     mtu = rxStream.mtu
+
+    # Allocate some buffers for each channel
+    # for some reason the compiler does not constant prop the type
+    # TODO put in signature
+    buffers = [Vector{T}(undef, mtu) for _ in 1:num_channels]
+    buff_ptrs = pointer(map(pointer, buffers))
 
     # output timings to console. Our austere TimerOutputs.jl
     last_timeoutput = get_time_us()
 
     # compute timeout based on sample_rate
-    timeout_estimate = uconvert(u"μs", buffsz/first(channels).sample_rate*2).val
+    timeout_estimate = uconvert(u"μs", mtu/first(channels).sample_rate*2).val
 
     # assert some properties
     # all sample rates should be the same
@@ -79,9 +74,6 @@ function record(output::AbstractString;
     end
     # convert fd to stdio, it is faster
     io = @ccall fdopen(io::Cint, "w"::Cstring)::Ptr{Cint}
-
-    # If there is timing slack, we can sleep a bit to run event handlers
-    have_slack = true
 
     # Our bespoke TimerOutputs.jl implementation
     timers = [0,0,0]
@@ -111,7 +103,7 @@ function record(output::AbstractString;
             temp_time = get_time_us()
             for sample in buffers
                 # size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
-                ret = @ccall fwrite(pointer(sample)::Ptr{Complex{Int16}}, 4::Csize_t, nread::Cint, io::Ptr{Cint})::Csize_t
+                ret = @ccall fwrite(pointer(sample)::Ptr{T}, sizeof(T)::Csize_t, nread::Cint, io::Ptr{Cint})::Csize_t
                 if ret < 0
                     error("Error writing to file: $ret")
                 end
@@ -119,7 +111,7 @@ function record(output::AbstractString;
             allocations[2] += Base.gc_bytes() - temp_bytes
             timers[2] = get_time_us() - temp_time
 
-            if have_slack && timer_display
+            if timer_display
                 temp_bytes = Base.gc_bytes()
                 temp_time = get_time_us()
                 begin
@@ -134,9 +126,6 @@ function record(output::AbstractString;
                 end
                 allocations[3] += Base.gc_bytes() - temp_bytes
                 timers[3] = get_time_us() - temp_time
-            end
-            begin
-                have_slack && GC.gc(false)
             end
         end
     finally
