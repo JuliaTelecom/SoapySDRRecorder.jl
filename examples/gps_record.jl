f72252a08683c5418f45b00ddbf41e44cbee7ff8
Steve Kelly <kd2cca@gmail.com>
2022-12-21 17:06:52 -0500

    csv logging

--- examples/gps_record.jl
+++ examples/gps_record.jl
@@ -1,17 +1,26 @@
-using SoapySDR, SoapySDRRecorder, Unitful
+using SoapyRTLSDR_jll, SoapySDR, SoapySDRRecorder, Unitful
 
 # Channel confuguration callback
 function configuration(dev, chans)
 
-    #dev[SoapySDR.Setting("biastee")] = "true"
+    dev[SoapySDR.Setting("biastee")] = "true"
 
     for rx in chans
-        rx.sample_rate = 2u"MHz"
+        rx.sample_rate = 3u"MHz"
+        rx.bandwidth = 3u"MHz"
         rx.frequency = 1_575_420_000u"Hz"
+        rx.gain_mode = true
         @show rx
     end
 
 end
 
-SoapySDRRecorder.record("test", device=Device(SoapySDR.Devices()[1]), channel_configuration=configuration)
+function telemetry_callback(dev, chans)
+    # this is terribly low level, but for some reason the highlevel API
+    # segfaults julia, so we keep it all in C
+    #@ccall printf("%s\n"::Cstring, SoapySDR.SoapySDRDevice_readSetting(dev.ptr, "DMA_BUFFERS")::Ptr{Cstring})::Cint
+end
+
+SoapySDRRecorder.record("test", device=Device(SoapySDR.Devices()[1]), channel_configuration=configuration,
+    telemetry_callback=telemetry_callback, compress=true, compression_level=5) # timeout=100000)
 
--- src/SoapySDRRecorder.jl
+++ src/SoapySDRRecorder.jl
@@ -25,9 +25,13 @@ function record(output::AbstractString;
                 channels::Union{Nothing, AbstractArray{<:SoapySDR.Channel}}=nothing,
                 channel_configuration::Union{Nothing, Function}=nothing,
                 telemetry_callback::Union{Nothing, Function}=nothing,
+                csv_log_callback::Union{Nothing, Function}=nothing,
+                csv_header_callback::Union{Nothing, Function}=nothing,
                 timeout = nothing,
                 compress = false,
                 compression_level = 0,
+                csv_log = true,
+                show_timer_stats = true,
                 stream_type::Type{T}=Complex{Int16}) where T
 
     # open the first device
@@ -66,8 +70,7 @@ function record(output::AbstractString;
     buffers = [Vector{T}(undef, mtu) for _ in 1:num_channels]
     buff_ptrs = pointer(map(pointer, buffers))
 
-    # output timings to console. Our austere TimerOutputs.jl
-    last_timeoutput = get_time_us()
+
 
     # compute timeout based on sample_rate, in integer microseconds for Soapy
     # note that poll has milisecond precision, so manually overriding this
@@ -84,8 +87,20 @@ function record(output::AbstractString;
     # all sample rates should be the same
     @assert all(c -> c.sample_rate == first(channels).sample_rate, channels)
 
+    # Our bespoke TimerOutputs.jl implementation
+    timers = [0,0,0,0]
+    allocations = [0,0,0,0]
+    temp_bytes = 0
+    temp_time = 0
+
+    # event counters and stats for stream reading
+    num_bufs_read = 0
+    num_timeouts = 0
+    num_overflows = 0
+
     # open up the output file
     io = Ptr{Cint}[]
+    csv_log_io = Ptr{Cint}(0)
     compress_io = GZipStream[]
     for i in 1:num_channels
         output_base = abspath(output)*"."*string(i)*".dat"
@@ -101,13 +116,23 @@ function record(output::AbstractString;
             push!(compress_io, GZip.open(output_base*".gz", "w"*string(compression_level)))
         end
     end
+    if csv_log
+        csv_log_file = abspath(output)*".csv"
+        touch(csv_log_file)
+        csv_log_io = @ccall open(csv_log_file::Cstring, 1::Cint)::Cint
+        if csv_log_io < 0
+            error("Error opening file, returned: $csv_log_io")
+        end
+        # convert fd to stdio, it is faster
+        csv_log_io = @ccall fdopen(csv_log_io::Cint, "w"::Cstring)::Ptr{Cint}
+        @ccall fprintf(csv_log_io::Ptr{Cint}, "time_us,num_bufs_read,num_overflows,num_timeouts,"::Cstring, get_time_us()::Int, num_bufs_read::Int, num_overflows::Int, num_timeouts::Int)::Cint
+        csv_header_callback !== nothing && csv_header_callback(csv_log_io, device, channels)
+        @ccall fprintf(csv_log_io::Ptr{Cint}, "\n"::Cstring)::Cint
+    end
 
-    # Our bespoke TimerOutputs.jl implementation
-    timers = [0,0,0]
-    allocations = [0,0,0]
-    temp_bytes = 0
-    temp_time = 0
-    num_bufs_read = 0
+    # output timings to console. Our austere TimerOutputs.jl
+    last_timeoutput = get_time_us()
+    last_csvoutput = get_time_us()
 
     # Enable ther stream
     @info "streaming..."
@@ -127,6 +152,7 @@ function record(output::AbstractString;
             if nread == SoapySDR.SOAPY_SDR_TIMEOUT
                 @ccall printf("T"::Cstring)::Cint
                 #@ccall _flushlbf()::Cvoid
+                num_timeouts += 1
                 allocations[1] += Base.gc_bytes() - temp_bytes
                 timers[1] += get_time_us() - temp_time
                 continue
@@ -135,6 +161,7 @@ function record(output::AbstractString;
                 @ccall printf("O"::Cstring)::Cint
                 #@ccall _flushlbf()::Cvoid
                 nread = mtu
+                num_overflows += 1
             elseif nread < 0
                 error("Error reading from stream: $(SoapySDR.SoapySDR_errToStr(nread))")
             end # else nread is the number of samples read, write to file next
@@ -159,6 +186,22 @@ function record(output::AbstractString;
             allocations[2] += Base.gc_bytes() - temp_bytes
             timers[2] += get_time_us() - temp_time
 
+            if csv_log
+                temp_bytes = Base.gc_bytes()
+                temp_time = get_time_us()
+                begin
+                    if get_time_us() - last_csvoutput > 1_000_000
+                        # We will log some stats here, then let the callback add things.
+                        @ccall fprintf(csv_log_io::Ptr{Cint}, "%ld,%ld,%ld,%ld,"::Cstring, get_time_us()::Int, num_bufs_read::Int, num_overflows::Int, num_timeouts::Int)::Cint
+                        csv_log_callback !== nothing && csv_log_callback(csv_log_io, device, channels)
+                        @ccall fprintf(csv_log_io::Ptr{Cint}, "\n"::Cstring)::Cint
+                        last_csvoutput = get_time_us()
+                    end
+                end
+                allocations[3] += Base.gc_bytes() - temp_bytes
+                timers[3] += get_time_us() - temp_time
+            end
+
             if timer_display
                 temp_bytes = Base.gc_bytes()
                 temp_time = get_time_us()
@@ -166,18 +209,22 @@ function record(output::AbstractString;
                     if get_time_us() - last_timeoutput > 1_000_000
                         telemetry_callback !== nothing && telemetry_callback(device, channels)
                         # some hackery to not allocate on the Julia GC so we use libc printf
-                        @ccall printf("Read Stream: %ld μs, expected time: %ld μs, net allocations: %ld bytes\n"::Cstring, timers[1]::Int, timeout_estimate::Cint, allocations[1]::Int)::Cint
-                        @ccall printf("Write File: %ld μs, net allocations: %ld bytes\n"::Cstring, timers[2]::Int, allocations[2]::Int)::Cint
-                        @ccall printf("Telemetry: %ld μs, net allocations: %ld bytes\n"::Cstring, timers[3]::Int, allocations[3]::Int)::Cint
-                        @ccall printf("Number of Buffers read: %ld\n"::Cstring, num_bufs_read::Int)::Cint
+                        if show_timer_stats
+                            @ccall printf("Read Stream: %ld μs, net allocations: %ld bytes\n"::Cstring, timers[1]::Int, allocations[1]::Int)::Cint
+                            @ccall printf("Write File: %ld μs, net allocations: %ld bytes\n"::Cstring, timers[2]::Int, allocations[2]::Int)::Cint
+                            @ccall printf("CSV Log: %ld μs, net allocations: %ld bytes\n"::Cstring, timers[3]::Int, allocations[3]::Int)::Cint
+                            @ccall printf("Telemetry: %ld μs, net allocations: %ld bytes\n"::Cstring, timers[4]::Int, allocations[4]::Int)::Cint
+                        end
+                        @ccall printf("Number of Buffers read: %ld Number of overflows: %ld Number of timeouts: %ld\n"::Cstring, num_bufs_read::Int, num_overflows::Int, num_timeouts::Int)::Cint
                         @ccall _flushlbf()::Cvoid
                         last_timeoutput = get_time_us()
                         timers .= 0
                     end
                 end
-                allocations[3] += Base.gc_bytes() - temp_bytes
-                timers[3] += get_time_us() - temp_time
+                allocations[4] += Base.gc_bytes() - temp_bytes
+                timers[4] += get_time_us() - temp_time
             end
+
         end
     finally
         SoapySDR.deactivate!(rxStream)
@@ -185,6 +232,7 @@ function record(output::AbstractString;
             @ccall close(io[i]::Cint)::Cint
             compress && close(compress_io[i])
         end
+        @ccall close(csv_log_io[i]::Cint)::Cint
     end
 end
 
